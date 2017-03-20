0970ca721655050fd08fa9a657056b0b00dd58ae
Jody Vanden <jody.vanden@gmail.com>
2017-03-20 17:58:57 +0100

    Add styleguide

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!
 
   def home
+  end
+
+  def styleguide
+    @base_font   = "Open Sans"
+    @header_font = "Raleway"
   end
 end
