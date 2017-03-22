32d79ed8a944914163b7ee32c987769d01da5d87
Amaury Debiard <amaury.debiard@ieseg.fr>
2017-03-22 17:18:00 +0100

    Fix validation on race for uniqueness

--- app/models/race.rb
+++ app/models/race.rb
@@ -6,7 +6,7 @@ class Race < ApplicationRecord
   has_many :race_sports
   has_many :sports, through: :race_sports
 
-  validates :name, uniqueness: ( scope: :date), presence: true
+  validates :name, uniqueness: { scope: :date}, presence: true
   validates :date, presence: true
   validates :place, presence: true
   validates :price, presence: true
