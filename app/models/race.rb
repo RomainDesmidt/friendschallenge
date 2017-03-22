51478f4c10109badc349fbc4f3f90640e3002b71
Jody Vanden <jody.vanden@gmail.com>
2017-03-22 16:21:54 +0100

    validates for model User, Races, Sport

--- app/models/race.rb
+++ app/models/race.rb
@@ -5,4 +5,10 @@ class Race < ApplicationRecord
   has_many :members, through: :group_memberships, source: :user, class_name: "User"
   has_many :race_sports
   has_many :sports, through: :race_sports
+
+  validates :name, uniqueness: ( scope: :date), presence: true
+  validates :date, presence: true
+  validates :place, presence: true
+  validates :price, presence: true
+  validates :place, presence: true
 end
