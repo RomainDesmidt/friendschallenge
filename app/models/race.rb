41d0e538dd781ec5d9d30c16e3c48605628e023d
Amaury Debiard <amaury.debiard@ieseg.fr>
2017-03-22 17:10:38 +0100

    WIP total distance computation for races

--- app/models/race.rb
+++ app/models/race.rb
@@ -3,4 +3,8 @@ class Race < ApplicationRecord
   has_many :users, through: :user_races
   has_many :race_sports
   has_many :sports, through: :race_sports
+
+  def calculate_total_distance!
+    self.total_distance = self.race_sports.pluck(:distance).reduce(:+)
+  end
 end
