ef7aef2d4aa3f9d6af195cd17a8085a2a3e4fbf4
Amaury Debiard <amaury.debiard@ieseg.fr>
2017-03-23 16:36:01 +0100

    Add coordinates to races

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322161822) do
+ActiveRecord::Schema.define(version: 20170323145247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20170322161822) do
     t.string   "status"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "sports", force: :cascade do |t|
