3f8127081548ae06744711efdf81102255a7f5f7
Amaury Debiard <amaury.debiard@ieseg.fr>
2017-03-23 15:16:35 +0100

    geocoding

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -2,8 +2,16 @@ class RacesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @races = Race.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@races) do |race, marker|
+      marker.lat race.latitude
+      marker.lng race.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
   end
 
   def show
   end
+
 end
