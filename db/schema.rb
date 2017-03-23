46df8a931829e497a80f9b600eb761103633fd16
Jody Vanden <jody.vanden@gmail.com>
2017-03-23 17:39:48 +0100

    Better name for place column on races => location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323140242) do
+ActiveRecord::Schema.define(version: 20170323163744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20170323140242) do
   create_table "races", force: :cascade do |t|
     t.text     "description"
     t.string   "name"
-    t.string   "place"
+    t.string   "location"
     t.float    "total_distance"
     t.date     "date"
     t.integer  "price"
