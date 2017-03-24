ed4dbb06161cb91f1a67276462c4b8d7f8b7599d
Amaury Debiard <amaury.debiard@ieseg.fr>
2017-03-24 18:40:06 +0100

    add windowinfo to map

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -5,10 +5,13 @@ class RacesController < ApplicationController
     @races = Race.where.not(latitude: nil, longitude: nil)
 
     @place_markers_hash = Gmaps4rails.build_markers(@races) do |race, marker|
-      marker.lat race.latitude
-      marker.lng race.longitude
+      marker.lat        race.latitude
+      marker.lng        race.longitude
+      marker.title      race.name
+      marker.infowindow render_to_string(partial: "/races/map_infowindow", locals:  { race: race })
       # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
+
   end
 
   def show
