04f87c0e366aeaa6a1b94c906c3f2b7f1ba35e74
Jody Vanden <jody.vanden@gmail.com>
2017-03-24 16:54:49 +0100

    add @race and @races to Race controller for different views

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -2,8 +2,10 @@ class RacesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @races = Race.all
   end
 
   def show
+    @race = Race.find(params[:id])
   end
 end
