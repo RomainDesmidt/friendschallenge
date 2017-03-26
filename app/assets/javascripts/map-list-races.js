8730bd9264062984a3b02c8d9695b5aca6906336
Jody Vanden <jody.vanden@gmail.com>
2017-03-26 16:11:45 +0200

    redesigned Races/index and Race/show

--- app/assets/javascripts/map-list-races.js
+++ app/assets/javascripts/map-list-races.js
@@ -1,13 +1,13 @@
-// listen for scroll
-var positionElementInPage = $('#map').offset().top;
-$(window).scroll(
-  function() {
-    // if ($(window).scrolldown(){
-    //   // fixed
-    //   $('#map').addClass("floatable");
-    // } else {
-      // relative
-      // $('#map').removeClass("floatable");
-    // }
-  s}
-);
+// // listen for scroll
+// var positionElementInPage = $('#map').offset().top;
+// $(window).scroll(
+//   function() {
+//     // if ($(window).scrolldown(){
+//     //   // fixed
+//     //   $('#map').addClass("floatable");
+//     // } else {
+//       // relative
+//       // $('#map').removeClass("floatable");
+//     // }
+//   s}
+// );
