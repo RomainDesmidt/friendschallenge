108a44a201c9e1b9bc40ee24e745f22f9929d6a0
Jody Vanden <jody.vanden@gmail.com>
2017-03-28 18:14:49 +0200

    modify to deploy on clever

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -5,5 +5,6 @@ class GroupMembershipsController < ApplicationController
     @race_group = RaceGroup.find(params[:race_group_id])
     @new_member = GroupMembership.create(user:current_user, race_group: @race_group, status: "Intéressé" )
     redirect_to race_group_path(@race_group)
+  end
 
 end
