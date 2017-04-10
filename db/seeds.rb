ece1bb8c05c83ed9a61f59b6e248dbb868940d28
Romain DESMIDT <romain.desmidt@gmail.com>
2017-04-10 17:55:00 +0200

    refacto seed easier to read User Creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require_relative "seed_user"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -53,235 +54,22 @@ end
 
 user_levels = ["débutant", "intermédiaire", "expert"]
 
-users_attributes = [
-  {
-    attributes: {
-      first_name: "Jane",
-      last_name: "Smith",
-      address: "1 Square de Roc Épine 49100 Angers",
-      uid: "",
-      email: "jane@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2016,2,25),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Natation", "Course à pied"]
-  },
-
-  {
-    attributes: {
-      first_name: "John",
-      last_name: "Dubois",
-      address: "4 Place du Dr Faust 85000 La Roche-sur-Yon",
-      uid: "",
-      email: "john@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2017,1,15),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Cyclisme", "Course à pied"]
-
-  },
-
-  {
-    attributes: {
-      first_name: "Lee",
-      last_name: "Sue",
-      address: "16 Rue de Kerozen 56000 Vannes",
-      uid: "",
-      email: "lee@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2016,6,23),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Cyclisme"]
-  },
-
-  {
-    attributes: {
-      first_name: "Alexandre",
-      last_name: "Legrand",
-      address: "3 Rue Raymond Marcellin 56370 Sarzeau",
-      uid: "",
-      email: "chuck@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2017,2,11),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Natation", "Cyclisme"]
-  },
-
-  {
-    attributes: {
-      first_name: "Romain",
-      last_name: "Perrin",
-      address: "1 Rue du Colonel Lebel 49300 Cholet",
-      uid: "",
-      email: "larry@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2016,9,30),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Natation"]
-  },
-
-  {
-    attributes: {
-      first_name: "Charles",
-      last_name: "Lacoche",
-      address: "20 rue de madrid 75008 Paris",
-      uid: "",
-      email: "charles@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2017,12,11),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Course à pied"]
-  },
-
-  {
-    attributes: {
-      first_name: "Tony",
-      last_name: "Mathieu",
-      address: "42 Rue Père Chevrier 69007 Lyon",
-      uid: "",
-      email: "tony@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2017,11,3),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Cyclisme"]
-  },
-
-  {
-    attributes: {
-      first_name: "Kylian",
-      last_name: "Fontaine",
-      address: "4 Rue Georges Courteline 31100 Toulouse",
-      uid: "",
-      email: "kylian@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2017,4,10),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Cyclisme", "Course à pied"]
-  },
-
-  {
-    attributes: {
-      first_name: "Yoann",
-      last_name: "James",
-      address: "117 Rue Hector Berlioz 44300 Nantes",
-      uid: "",
-      email: "lebron@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2017,9,3),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Natation", "Cyclisme"]
-  },
-
-  {
-    attributes: {
-      first_name: "Michael",
-      last_name: "Chevalier",
-      address: "44 Rue de la Ripossière 44200 Nantes",
-      uid: "",
-      email: "michael@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2017,6,22),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Course à pied"]
-  },
-
-  {
-    attributes: {
-      first_name: "David",
-      last_name: "Martinez",
-      address: "14 Rue de la Belle Olonnaise 85340 Olonne-sur-Mer",
-      uid: "",
-      email: "david@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2017,3,15),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Cyclisme"]
-  },
-
-  {
-    attributes: {
-      first_name: "Jody",
-      last_name: "Vanden",
-      address: "82 Boulevard Pierre de Coubertin 44800 Saint-Herblain",
-      uid: "",
-      email: "jody@gmail.com",
-      password: "123456",
-      password_confirmation: "123456",
-      medical_certificate_date: Date.new(2017,9,15),
-      level: user_levels.sample
-    },
-    medical_certificate: File.new(Rails.root.join("db/fixtures/images/certificat_medical/certificat_medical.png")),
-    profile_picture: "http://lorempixel.com/300/300/people/#{rand(40) + 1}/",
-    sports_names: ["Natation", "Course à pied"]
-  }
-]
 
 
 users_attributes.each do |user_attributes|
-  #puts user_attributes[:attributes][:first_name]
   user = User.new(user_attributes[:attributes])
-  user.medical_certificate = user_attributes[:medical_certificate]
-  user.profile_picture = user_attributes[:profile_picture]
-  #sleep 0.8
-  user.save
+  user.save!
+  p user.name
 
   sports_names = user_attributes[:sports_names]
   unless sports_names.nil?
     sports_names.each do |sport_name|
-      sport = Sport.find_by_name(sport_name)
-      UserSport.create(user: user, sport: sport)
+      sport_object = Sport.find_by_name(sport_name)
+      UserSport.create(user: user, sport: sport_object)
     end
   end
 end
-User.all.each do |user|
-p user.first_name
-p user.medical_certificate
-p user.profile_picture
-end
+
 
 # # /////////////////////////////////////////////////////////////////////////////////////////////
 # # ////////////////////////////////////////////////////////////////////////////////////////////
